036b416cdc2cdd17538cb3ed48c9657a7bcaca2a
michael <kurashevmichael@gmail.com>
2022-11-23 14:21:54 +0300

    update lesson_6

--- Django_Rest_Framework_GB/settings.py
+++ Django_Rest_Framework_GB/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'authapp',
     'corsheaders',
     'todo',
-    'django_filters'
+    'django_filters',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -134,5 +135,13 @@ CORS_ALLOWED_ORIGINS = [
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAdminUser'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ]
 }
