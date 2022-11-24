2dcc79c26de88569bcf984c143f1b1bfbb855236
michael <kurashevmichael@gmail.com>
2022-11-24 14:22:45 +0300

    lesson_7

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,11 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import NotFound from "./components/NotFound";
 import MenuItem from "./components/Menu";
 import FooterItem from "./components/Footer";
+import LoginForm from "./components/Auth";
+import Cookies from "universal-cookie";
+
+/* создаем класс (обьект, компонент) с состоянием (информация).
+Тут будет храниться инфа и передаваться в др. компоненты */
 
 class App extends React.Component {
     constructor(props) {
@@ -17,13 +22,16 @@ class App extends React.Component {
             'users': [],
             'notes': [],
             'projects': [],
+            'token': ''
         }
 
     }
 
-    componentDidMount() {
+/* Делаем запросы на бек (добавляем токен headers вторым параметром) */
 
-        axios.get('http://127.0.0.1:8000/api/users/')
+    loadData() {
+        const headers = this.getHeaders()
+        axios.get('http://127.0.0.1:8000/api/users/', {headers})
             .then(response => {
                 const users = response.data
                 this.setState(
@@ -32,9 +40,12 @@ class App extends React.Component {
                     }
                 )
 
-            }).catch(error => console.log(error))
+            }).catch(error => {
+                console.log(error)
+                this.setState({users: []})
+        })
 
-        axios.get('http://127.0.0.1:8000/api/todo/')
+        axios.get('http://127.0.0.1:8000/api/todo/', {headers})
             .then(response => {
                 const notes = response.data
                 this.setState(
@@ -43,9 +54,12 @@ class App extends React.Component {
                     }
                 )
 
-            }).catch(error => console.log(error))
+            }).catch(error => {
+                console.log(error)
+                this.setState({notes: []})
+        })
 
-        axios.get('http://127.0.0.1:8000/api/projects/')
+        axios.get('http://127.0.0.1:8000/api/projects/', {headers})
             .then(response => {
                 const projects = response.data
                 this.setState(
@@ -54,19 +68,81 @@ class App extends React.Component {
                     }
                 )
 
-            }).catch(error => console.log(error))
-
+            }).catch(error => {
+            console.log(error)
+            this.setState({projects: []})
+        })
     }
+
+/* Этот код выполняется каждый раз, когда запускается сервер (делает запросы на сервер + достает токен из cookies и
+* определяет авторизирован пользователь или нет) */
+
+    componentDidMount() {
+        this.getTokenFromStorage()
+    }
+
+/* Этот метод создает и получает токен авторизации */
+
+    getToken(username, password) {
+        axios.post('http://127.0.0.1:8000/api_token/', {username: username, password: password})
+            .then(response => {
+                this.setToken(response.data['token'])
+            }).catch(error => alert('Неверный логин или пароль'))
+    }
+
+/* Этот метод принимает токен, устанавливает его в cookies и записывает состояние приложения */
+
+    setToken(token) {
+        const cookies = new Cookies()
+        cookies.set('token', token)
+        this.setState({'token': token}, ()=>this.loadData())
+    }
+
+/* Этот метод добавляет токен в headers, если пользователь авторизирован */
+
+    getHeaders() {
+        let headers = {
+            'Content-Type': 'application/json'
+        }
+        if (this.isAuthenticated()){
+            headers['Authorization'] = 'Token ' + this.state.token
+        }
+        return headers
+    }
+
+/* Этот метод проверяет авторизирован пользователь или нет */
+
+    isAuthenticated() {
+        return this.state.token !== ''
+    }
+
+/* Этот метод будет обнулять токен в cookies */
+
+    logout() {
+        this.setToken('')
+    }
+
+/* Считывает токен из cookies и записывает его состояние (нужен, когда мы снова открываем страницу сайта) */
+
+    getTokenFromStorage() {
+        const cookies = new Cookies()
+        const token = cookies.get('token')
+        this.setState({'token':token}, ()=>this.loadData())
+    }
+
+/* Эта функция создает и возвращает верстку js */
 
     render() {
         return (
             <div>
                 <BrowserRouter>
-                    <MenuItem/>
+                    <MenuItem parentComponent={this}/>
                     <Routes>
                         <Route path='/' element={<UsersList users={this.state.users}/>}/>
                         <Route path='/todo' element={<NotesList notes={this.state.notes}/>}/>
                         <Route path='/projects' element={<ProjectsList projects={this.state.projects}/>}/>
+                        <Route path='/login' element={<LoginForm
+                            getToken={(username, password) => this.getToken(username, password)}/>}/>
                         <Route path='*' element={<NotFound/>}/>
                     </Routes>
                     <FooterItem/>
--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import {Link} from "react-router-dom";
 
-const MenuItem = () => {
+
+const MenuItem = (component) => {
     return (
         <nav>
             <ul>
@@ -13,6 +14,11 @@ const MenuItem = () => {
                 </li>
                 <li>
                     <Link to='/projects'>Projects</Link>
+                </li>
+                <li>
+                    {component.parentComponent.isAuthenticated() ?
+                        <button onClick={()=>component.parentComponent.logout()}>Logout</button> :
+                        <Link to='/login'>Login</Link>}
                 </li>
             </ul>
         </nav>
